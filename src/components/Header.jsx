74414a2d057d42b8fc381d97d56fdf2687b97a85
muzammilnawaz <muzammilnawaz2935337@gmail.com>
2025-05-10 11:29:01 -0700

    updated coded

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,60 @@
-// Header.jsx
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 const Header = () => {
+
+  const [isShow , setIsShow] = useState(false);
+  console.log(isShow)
   return (
+   <>
     <header
       id="header"
-      className="text-white sticky top-0 left-0 z-50 h-[60px] flex justify-between items-center px-[120px]"
-    >
+      className={`text-white  sticky top-0 left-0 h-[60px] flex justify-between items-center px-[20px] lg:px-[100px]`}
+     >
       <div>
         <img src="/images/logo.png" alt="Logo" />
       </div>
-      <div className="flex justify-between gap-x-4 items-center capitalize text-[18px] font-light">
-        <ul><Link to="/home">Home</Link></ul>
-        <ul><Link to="/home">About</Link></ul>
-        <ul><Link to="/home">Service</Link></ul>
-        <ul><Link to="/home">Unique</Link></ul>
-        <ul><Link to="/home">Feature</Link></ul>
-        <ul><Link to="/home">Review</Link></ul>
-        <ul><Link to="/home">Faq</Link></ul>
+      <div className="hidden lg:flex justify-between gap-x-4 items-center capitalize text-[20px] font-light list-none">
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/home">About</Link></li>
+        <li><Link to="/home">Service</Link></li>
+        <li><Link to="/home">Unique</Link></li>
+        <li><Link to="/home">Feature</Link></li>
+        <li><Link to="/home">Review</Link></li>
+        <li><Link to="/home">Faq</Link></li>
         <select className="cursor-pointer bg-transparent border-0 outline-0 py-1 px-3">
           <option value="" hidden>Pages</option>
           <option className="text-black">Generics</option>
           <option className="text-black">Element</option>
         </select>
       </div>
+        <span onClick={() => setIsShow(prev => !prev)} className='text-3xl lg:hidden cursor-pointer'>{isShow ? null : <IoMenu/>}</span>
     </header>
+    {(isShow )? 
+    <div onClick={() => setIsShow(false)} className='lg:hidden h-[100vh] z-[100] w-[100%] BGlayer fixed top-0 left-0 '> 
+    <span className='absolute z-[100] text-4xl right-0 py-4 px-4 cursor-pointer' onClick={() => setIsShow(false)}><IoMdClose/></span>
+      <div onClick={(e) =>e.stopPropagation()} className='lg:hidden w-[25%] z-[-100]  bgleft h-[100vh] scroll-hidden flex justify-start px-5 items-start py-6 text-2xl'>
+      <div className='flex flex-col '>
+        <ul className='flex font-extrabold text-xl  flex-col  gap-y-4 py-4'>
+        <li><Link to="/home">Home</Link></li>
+        <li><Link to="/home">About</Link></li>
+        <li><Link to="/home">Service</Link></li>
+        <li><Link to="/home">Unique</Link></li>
+        <li><Link to="/home">Feature</Link></li>
+        <li><Link to="/home">Review</Link></li>
+        <li><Link to="/home">Faq</Link></li>
+        <select className="cursor-pointer bg-transparent border-0 outline-0">
+          <option value="" hidden>Pages</option>
+          <option className="text-black">Generics</option>
+          <option className="text-black">Element</option>
+        </select>
+        </ul>
+        
+      </div>
+    </div>
+    </div>: null}
+   </>
   );
 };
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import Header from '../components/Header';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
-import '../App.css'; // Make sure this is imported
+import '../App.css';
 
 const Home = () => {
   useEffect(() => {
@@ -29,16 +29,25 @@ const Home = () => {
       <div id="main" className="min-h-screen text-white">
       <Header />
         <div className="flex flex-col items-center py-16 gap-y-4">
-          <p className="text-2xl">Now you can feel the Heat</p>
-          <h1 className="text-7xl font-extrabold">Smart New Future</h1>
+          <p className="text-[20px] sm:text-2xl md:text-xl">Now you can feel the Heat</p>
+          <h1 className="text-[30px] sm:text-7xl w-full text-center  font-extrabold">Smart New Future</h1>
           <button className="rounded-3xl px-7 text-[15px] hover:bg-transparent hover:text-white hover:border border-yellow-50 text-black py-2 bg-white">
             BUY NOW
           </button>
         </div>
       </div>
 
-      <div className="h-[300vh] bg-red-950 text-white text-center text-3xl pt-20">
-        Scroll down and see the header color go black 🖤
+      <div className='h-screen flex justify-center items-center px-20 gap-x-4'>
+       <div className='flex flex-col justify-center items-start gap-y-6'>
+         <p className='text-xl font-thin text-blue-500 '>Brand new app to blow your mind</p>
+        <h1 className='text-5xl font-extrabold text-wrap'>We’ve made a life that will change you</h1>
+        <p className='text-xl font-normal text-black'>We are here to listen from you deliver exellence</p>
+        <p className='text-[16px] text-wrap text-start capitalize font-thin text-slate-400'>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod temp or incididunt ut labore et dolore magna aliqua. Ut enim ad minim.</p>
+        <button className='bg-[#222222] hover:bg-transparent border border-[#222222] hover:text-[#222222] font-thin text-white uppercase text-[15px] py-1 px-7 rounded-full'>Get Started now</button>
+       </div>
+       <div>
+        <img  className='h-[310px] w-[800px]' src="./public/images/video-bg.png" alt="" />
+       </div>
       </div>
     </div>
   );
